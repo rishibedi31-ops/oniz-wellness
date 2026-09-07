68a07f20237ba192575f6b3c2063272d910bfd45
rishibedi31-ops <rishibedi31@gmail.com>
2026-09-07 17:01:44 -0400

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-window.ONIZ = {
-  api: "https://onizwellness.rishibedi31.workers.dev",
-  name: "Oniz Health and Wellness",
-  email: "hello@onizhealthwellness.ca",
-  phone: "(905) 555-0148",
-  tz: "America/Toronto",
-};
